def9c5399cd17a493777a5e1cdd8702207d6e2af
Rashedul Islam <12345678+immense055@users.noreply.github.com>
2026-07-18 05:30:16 +0600

    Add database connection model

---
+++ models.py
@@ -0,0 +1,13 @@
+import pymysql
+from config import config
+
+def get_connection():
+    return pymysql.connect(
+        host=config.DB_HOST,
+        port=config.DB_PORT,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        database=config.DB_NAME,
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True
+    )
